e809471f62318b204000fd0367a4a0a8aa4e84db
Morgan Fudge <mfudge92@gmail.com>
2017-12-20 15:46:18 +0000

    automatically backed up by learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -11,8 +11,8 @@
 class CreateCostumes < ActiveRecord::Migration[4.2]
   def change
     create_table :costumes do |t|
-      t.string :name,
-      t.float :price,
+      t.string :name
+      t.float :price
       t.string :image_url
       t.size :string
     end
