52a67cf0aed1d27e6d46a136a863d1e484d89369
Morgan Fudge <mfudge92@gmail.com>
2017-12-20 15:54:03 +0000

    automatically backed up by learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -15,6 +15,8 @@ class CreateCostumes < ActiveRecord::Migration[4.2]
       t.float :price
       t.string :image_url
       t.string :size
+      t.datetime :created
+      t.datetime :updated
     end
   end
 end
