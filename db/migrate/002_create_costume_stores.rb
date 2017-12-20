c813340d653358819bcf398bd6229d9c0d008ea9
Morgan Fudge <mfudge92@gmail.com>
2017-12-20 15:55:11 +0000

    automatically backed up by learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -8,6 +8,8 @@ class CreateCostumeStores < ActiveRecord::Migration[4.2]
       t.string :inventory
       t.integer :employees
       t.boolean :still_in_business
+      t.datetime :opening
+      t.datetime :closing
     end
   end
 end
