33e62090eb3d3092c4b92a6ca8d2b537e0906154
Morgan Fudge <mfudge92@gmail.com>
2017-12-20 15:59:21 +0000

    automatically backed up by learn

--- db/schema.rb
+++ db/schema.rb
@@ -15,11 +15,11 @@ ActiveRecord::Schema.define(version: 3) do
   create_table "costume_stores", force: :cascade do |t|
     t.string "name"
     t.string "location"
-    t.string "inventory"
+    t.string "costume_inventory"
     t.integer "num_of_employees"
     t.boolean "still_in_business"
-    t.datetime "opening"
-    t.datetime "closing"
+    t.datetime "opening_time"
+    t.datetime "closing_time"
   end
 
   create_table "costumes", force: :cascade do |t|
