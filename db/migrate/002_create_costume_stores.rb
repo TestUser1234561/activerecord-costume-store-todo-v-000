aadb0ebcab6667aa8caed70dcedcd40309c494e1
Morgan Fudge <mfudge92@gmail.com>
2017-12-20 15:58:36 +0000

    automatically backed up by learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -5,11 +5,11 @@ class CreateCostumeStores < ActiveRecord::Migration[4.2]
     create_table :costume_stores do |t|
       t.string :name
       t.string :location
-      t.string :inventory
+      t.string :costume_inventory
       t.integer :num_of_employees
       t.boolean :still_in_business
-      t.datetime :opening
-      t.datetime :closing
+      t.datetime :opening_time
+      t.datetime :closing_time
     end
   end
 end
